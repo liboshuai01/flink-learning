e68f3bcc6813c64ebb125b117311e74c84aea805
BoShuai Li <liboshuai01@gmail.com>
2024-12-03 10:05:26 +0800

    feat(flink-learning-netty-demo2): netty客户端与服务端双向通信 2.0
    我们用一个非常简单的 Demo 来了解一下服务端和客户端是如何来通信的

---
+++ flink-learning-netty/flink-learning-netty-demo02/src/main/java/cn/liboshuai/flink/netty/demo2/server/FirstServerHandler.java
@@ -0,0 +1,30 @@
+package cn.liboshuai.flink.netty.demo2.server;
+
+import cn.liboshuai.flink.netty.demo2.util.NettyUtils;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 服务端的自定义处理类，扩展自 Netty 的 ChannelInboundHandlerAdapter 类
+ */
+@Slf4j
+public class FirstServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 当读取到客户端数据时触发此方法
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg; // 将接收到的数据转换为 Netty 的 ByteBuf 类型
+        log.info("接收到来自客户端的数据：{}", byteBuf.toString(StandardCharsets.UTF_8)); // 打印接收的数据（字符串）
+
+        // 服务端向客户端发送响应消息
+        String message = "你好，我是服务端";
+        ByteBuf out = NettyUtils.convertString2ByteBuf(message, ctx); // 将字符串消息转换为 ByteBuf
+        ctx.channel().writeAndFlush(out); // 将响应消息写入通道并刷新
+    }
+}
--- flink-learning-netty/flink-learning-netty-demo02/src/main/java/cn/liboshuai/flink/netty/demo2/util/NettyUtils.java
+++ flink-learning-netty/flink-learning-netty-demo02/src/main/java/cn/liboshuai/flink/netty/demo2/util/NettyUtils.java
@@ -5,11 +5,16 @@ import io.netty.channel.ChannelHandlerContext;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * 工具类，包含与 Netty 字符串与 ByteBuf 的转换方法
+ */
 public class NettyUtils {
-    private ByteBuf convertString2ByteBuf(String str, ChannelHandlerContext ctx) {
-        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
-        ByteBuf byteBuf = ctx.alloc().buffer();
-        byteBuf.writeBytes(bytes);
-        return byteBuf;
+
+    // 将字符串转换为 Netty 的 ByteBuf
+    public static ByteBuf convertString2ByteBuf(String str, ChannelHandlerContext ctx) {
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8); // 将字符串编码为 UTF-8 字节数组
+        ByteBuf byteBuf = ctx.alloc().buffer(); // 分配一个 ByteBuf
+        byteBuf.writeBytes(bytes); // 将字节数组写入 ByteBuf
+        return byteBuf; // 返回封装后的 ByteBuf
     }
 }
